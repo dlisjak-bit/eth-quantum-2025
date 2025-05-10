c8ff659890fc4f6702fa34dddec9109d68de6470
dlisjak-bit <dlisjak121@gmail.com>
2025-05-10 15:04:03 +0200

    position drawing

--- qft/testing_positions.py
+++ qft/testing_positions.py
@@ -3,11 +3,13 @@ import os
 import pennylane as qml
 import numpy as np
 
-qftdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+qftdir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, qftdir)
 import trap
-position= [(0, 0), (0, 1), (1, 0), (1, 2), (2, 0), (2, 1), (3, 0), (3, 1)]
-trap_graph = trap.create_trap_graph()
-trap.plot_trap_graph(trap_graph)
-# trap.plot_trap_graph_positions(trap_graph, qubit_positions=position)
 
+
+position = [(1, 1), (1, 1), (1, 0), (1, 2), (2, 0), (2, 1), (3, 0), (3, 1, "idle")]
+trap_graph = trap.create_trap_graph()
+print(trap_graph.nodes(data=True))
+# trap.plot_trap_graph_positions(trap_graph, qubit_positions=position)
+# trap.plot_trap_graph_positions(trap_graph, qubit_positions=position)
