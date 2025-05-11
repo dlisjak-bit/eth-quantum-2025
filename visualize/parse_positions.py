382c396aa659852b9f7bff8ac5b59609fc6a454b
dlisjak-bit <dlisjak121@gmail.com>
2025-05-11 04:51:52 +0200

    working stupid game after a few hours

---
+++ visualize/parse_positions.py
@@ -0,0 +1,33 @@
+all = []
+N = 7
+with open("positions.csv", "r") as f:
+    for line in f:
+        timestep = []
+        if line.startswith("t"):
+            continue
+        line = line.strip().split(",")[1:]
+        for i in range(N):
+            x = int(line[i * 3])
+            y = int(line[i * 3 + 1])
+            idle = int(line[i * 3 + 2])
+            if idle == 0:
+                timestep.append((x, y))
+            else:
+                timestep.append((x, y, "idle"))
+        all.append(timestep)
+# print(all)
+for i, timestep in enumerate(all):
+    print(i, timestep)
+
+import numpy as np
+
+data = np.load("gates_schedule.npz", allow_pickle=True)
+print(data)
+gates = data["gates_schedule"]
+for i, timestep in enumerate(gates):
+    print(i, timestep)
+
+print("_--------------------")
+gates = data["conv_schedule"]
+for i, timestep in enumerate(gates):
+    print(i, timestep)
